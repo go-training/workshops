d62e2eecf9543e8fb2c3c645d3cbe0f26b78ee38
Bo-Yi Wu <appleboy.tw@gmail.com>
2017-05-20 11:42:28 +0800

    add const
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- 20170520/01-helloworld.go
+++ 20170520/01-helloworld.go
@@ -6,7 +6,27 @@ import (
 	helloworld "github.com/go-training/workshops/20170520/hello"
 )
 
+const (
+	A = iota + 1
+	B
+	C
+)
+
 func main() {
 	fmt.Println("Hello World!!")
 	fmt.Println(helloworld.HelloWorld())
+
+	var a int
+	var b = 1
+	c := 1
+	a = 1
+	hello := "Hello world"
+	fmt.Println("a:", a)
+	fmt.Println("b:", b)
+	fmt.Println("c:", c)
+	fmt.Println(hello)
+
+	fmt.Println("A:", A)
+	fmt.Println("B:", B)
+	fmt.Println("C:", C)
 }
