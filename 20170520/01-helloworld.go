b652f1d52ad01a023e21bf340bd4249b1d6e79f9
Bo-Yi Wu <appleboy.tw@gmail.com>
2017-05-20 11:34:54 +0800

    add helloworld.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- 20170520/01-helloworld.go
+++ 20170520/01-helloworld.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	helloworld "github.com/go-training/workshops/20170520/hello"
+)
 
 func main() {
 	fmt.Println("Hello World!!")
+	fmt.Println(helloworld.HelloWorld())
 }
---
+++ 20170520/hello/helloworld.go
@@ -0,0 +1,10 @@
+package hello
+
+// HelloWorld
+func HelloWorld() string {
+	return hello()
+}
+
+func hello() string {
+	return "hello world!!"
+}
